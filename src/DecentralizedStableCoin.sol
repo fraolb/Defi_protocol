992d70dc719a4723ff1bb21831ff59990aeca2d7
fraolb <fraolbereket@gmail.com>
2024-07-24 20:37:43 +0300

    building stable coin

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+/*
+ * @title Decentralized Stable Coin
+ * @author Fraol B
+ * Collateral: Exogenous (ETH, BTC)
+ * Minting: Algorithmic
+ * Relative Stability: Pagged to USD
+ * This is an exercise contract governed by DSCEngine. It uses ERC20.
+ */
+contract DecentralizedStableCoin {
+    constructor() {}
+}
