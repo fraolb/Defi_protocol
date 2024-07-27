db6363df879f08a0dcbf0fd60516683572d4d299
fraolb <fraolbereket@gmail.com>
2024-07-27 21:46:35 +0300

    more test

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
@@ -45,10 +45,10 @@ contract DSCEngineTest is Test {
     }
 
     ///////////////////////////
-    ////Pice Tests      //////
+    ////Price Tests      //////
     /////////////////////////
 
-    function testGetUsdValue() public {
+    function testGetUsdValue() public view {
         uint256 ethAmount = 15e18;
         uint256 expectedUsd = 30000e18;
 
@@ -57,7 +57,7 @@ contract DSCEngineTest is Test {
         assertEq(expectedUsd, actualUsd);
     }
 
-    function testGetTokenAmountFromUsd() public {
+    function testGetTokenAmountFromUsd() public view {
         uint256 usdAmount = 100 ether;
         // $2000 / Eth, $100
         uint256 expectedWeth = 0.05 ether;
@@ -102,5 +102,28 @@ contract DSCEngineTest is Test {
 
         assertEq(totalDscMinted, expectedTotalDscMinted);
         assertEq(AMOUNT_COLLATERAL, expectedCollateralValueInUsd);
+    }
+
+    function testDscMinted() public depositedCollateral {
+        vm.startPrank(USER);
+        (, uint256 collateralValueInUsd) = engine.getAccountInformation(USER);
+        uint256 collateralValue = engine.getTokenAmountFromUsd(weth, collateralValueInUsd);
+        uint256 expectedDscMinted = collateralValue / 2;
+
+        engine.mintDsc(expectedDscMinted);
+        (uint256 totalDscMinted,) = engine.getAccountInformation(USER);
+        console.log(totalDscMinted);
+        assertEq(totalDscMinted, expectedDscMinted);
+        vm.stopPrank();
+    }
+
+    function testDscMintFailForOverMint() public depositedCollateral {
+        vm.startPrank(USER);
+        (, uint256 collateralValueInUsd) = engine.getAccountInformation(USER);
+
+        vm.expectRevert(DSCEngine.DSCEngine__BreaksHealthFactor.selector);
+        engine.mintDsc(collateralValueInUsd);
+
+        vm.stopPrank();
     }
 }
